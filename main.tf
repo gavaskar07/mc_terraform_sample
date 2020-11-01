164cfcfce1332eb14ff8c73edd926d6f9de0f866
GAVASKAR <gavaskar07@gmail.com>
2020-11-01 11:51:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,28 +7,28 @@ terraform {
   }
 }
 
-resource "alicloud_vpc" "multicloud_vpc" {
+resource "alicloud_vpc" "vpc" {
   name       = "multicloud_vpc"
   cidr_block = "10.0.0.0/16"
 }
 
 resource "alicloud_vswitch" "mc-webapp" {
   name              = "Mc_Subnet_webapp"
-  vpc_id            = alicloud_vpc.multicloud_vpc.id
+  vpc_id            = alicloud_vpc.vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "	ap-south-1"
 }
 
 resource "alicloud_vswitch" "mc_mobileapp" {
   name              = "Mc_Subnet_mobileapp"
-  vpc_id            = alicloud_vpc.multicloud_vpc.id
+  vpc_id            = alicloud_vpc.vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "	ap-south-1"
 }
 
 resource "alicloud_vswitch" "mc_db" {
   name              = "Mc_Subnet_Db"
-  vpc_id            = alicloud_vpc.multicloud_vpc.id
+  vpc_id            = alicloud_vpc.vpc.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "	ap-south-1"
 }
