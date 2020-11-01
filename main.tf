27d85cacab4a92ab786987b68fd310a512910371
GAVASKAR <gavaskar07@gmail.com>
2020-11-01 12:07:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,27 +8,13 @@ terraform {
 }
 
 resource "alicloud_vpc" "vpc" {
-  name       = "tf_webinar_infra"
+  name       = "tcloud-resource"
   cidr_block = "10.0.0.0/16"
 }
 
 resource "alicloud_vswitch" "vsw-web" {
-  name              = "subnet-web"
+  name              = "subnet-tcloud"
   vpc_id            = alicloud_vpc.vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-southeast-1a"
-}
-
-resource "alicloud_vswitch" "subnet-app" {
-  name              = "subnet-app"
-  vpc_id            = alicloud_vpc.vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-southeast-1a"
-}
-
-resource "alicloud_vswitch" "subnet-db" {
-  name              = "subnet-db"
-  vpc_id            = alicloud_vpc.vpc.id
-  cidr_block        = "10.0.3.0/24"
   availability_zone = "ap-southeast-1a"
 }
