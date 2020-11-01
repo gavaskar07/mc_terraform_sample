100265c5210747acc6109aedf385574fe14503eb
GAVASKAR <gavaskar07@gmail.com>
2020-11-01 11:43:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,21 +14,21 @@ resource "alicloud_vpc" "multicloud_vpc" {
 
 resource "alicloud_vswitch" "mc-webapp" {
   name              = "Mc_Subnet_webapp"
-  vpc_id            = alicloud_vpc.vpc.id
+  vpc_id            = alicloud_vpc.multicloud_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-southeast-1a"
 }
 
 resource "alicloud_vswitch" "mc_mobileapp" {
   name              = "Mc_Subnet_mobileapp"
-  vpc_id            = alicloud_vpc.vpc.id
+  vpc_id            = alicloud_vpc.multicloud_vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "ap-southeast-1a"
 }
 
 resource "alicloud_vswitch" "mc_db" {
   name              = "Mc_Subnet_Db"
-  vpc_id            = alicloud_vpc.vpc.id
+  vpc_id            = alicloud_vpc.multicloud_vpc.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "ap-southeast-1a"
 }
